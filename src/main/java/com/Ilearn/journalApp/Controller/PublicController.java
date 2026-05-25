bf38be78e8644b855168f157d8280d5de331231b
SoubhagyaM <soubhagyametri333@gmail.com>
2026-05-25 16:02:43 +0530

    imported cros

--- src/main/java/com/Ilearn/journalApp/Controller/PublicController.java
+++ src/main/java/com/Ilearn/journalApp/Controller/PublicController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import com.Ilearn.journalApp.Entity.User;
 import com.Ilearn.journalApp.dto.UserDto;
 import com.Ilearn.journalApp.service.UserDetailsServiceImpl;
