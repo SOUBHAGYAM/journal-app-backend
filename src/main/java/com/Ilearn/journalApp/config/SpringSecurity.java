f7ce90bc664c7565e8951de096cbe5e3c3c8c1cc
SoubhagyaM <soubhagyametri333@gmail.com>
2026-05-25 15:45:17 +0530

    final production cors fix

--- src/main/java/com/Ilearn/journalApp/config/SpringSecurity.java
+++ src/main/java/com/Ilearn/journalApp/config/SpringSecurity.java
@@ -65,22 +65,27 @@ public class SpringSecurity {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(
-                List.of("http://localhost:4200")
+        configuration.setAllowedOriginPatterns(
+                List.of("*")
         );
 
         configuration.setAllowedMethods(
-                List.of("*")
+                List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")
         );
 
         configuration.setAllowedHeaders(
                 List.of("*")
         );
 
+        configuration.setAllowCredentials(false);
+
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
 
-        source.registerCorsConfiguration("/**", configuration);
+        source.registerCorsConfiguration(
+                "/**",
+                configuration
+        );
 
         return source;
     }
