9d254c80154a2d8a170b8061001220d25169e130
SoubhagyaM <soubhagyametri333@gmail.com>
2026-05-25 16:16:04 +0530

    simple code

--- src/main/java/com/Ilearn/journalApp/config/SpringSecurity.java
+++ src/main/java/com/Ilearn/journalApp/config/SpringSecurity.java
@@ -1,37 +1,13 @@
 package com.Ilearn.journalApp.config;
 
-import com.Ilearn.journalApp.service.UserDetailsServiceImpl;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import org.springframework.http.HttpMethod;
-
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
 import org.springframework.security.web.SecurityFilterChain;
 
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
 @Configuration
-@EnableWebSecurity
 public class SpringSecurity {
-
-    @Autowired
-    private UserDetailsServiceImpl userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(
@@ -39,88 +15,8 @@ public class SpringSecurity {
     ) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())
-
-                .authorizeHttpRequests(auth -> auth
-
-                        .requestMatchers(
-                                HttpMethod.OPTIONS,
-                                "/**"
-                        ).permitAll()
-
-                        .requestMatchers(
-                                "/public/**"
-                        ).permitAll()
-
-                        .requestMatchers(
-                                "/journal/**",
-                                "/user/**"
-                        ).permitAll()
-
-                        .requestMatchers(
-                                "/admin/**"
-                        ).hasRole("ADMIN")
-
-                        .anyRequest().permitAll()
-                )
-
-                .httpBasic(Customizer.withDefaults())
-
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(
-                                SessionCreationPolicy.STATELESS
-                        )
-                );
+            .csrf(csrf -> csrf.disable());
 
         return http.build();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-
-        CorsConfiguration configuration =
-                new CorsConfiguration();
-
-        configuration.setAllowedOriginPatterns(
-                List.of("*")
-        );
-
-        configuration.setAllowedMethods(
-                List.of("GET",
-                        "POST",
-                        "PUT",
-                        "DELETE",
-                        "OPTIONS")
-        );
-
-        configuration.setAllowedHeaders(
-                List.of("*")
-        );
-
-        configuration.setAllowCredentials(false);
-
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
-
-        source.registerCorsConfiguration(
-                "/**",
-                configuration
-        );
-
-        return source;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration authConfig
-    ) throws Exception {
-
-        return authConfig.getAuthenticationManager();
     }
 }
